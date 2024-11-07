2ba0e643f61127339bb15473af7db711ebf7adb2
Mel Padden <melpadden@gmail.com>
2024-11-07 21:43:00 +0100

    Add templates in ENV

--- scripts/build-staging.sh
+++ scripts/build-staging.sh
@@ -3,7 +3,7 @@ yarn clear
 cp ./env/staging.env ./.env
 cat ./env/algolia.env >> ./.env
 #cp ./env/staging.CNAME ./CNAME
-cp ./env/staging.robots.txt ./staticrobots.txt
+cp ./env/staging.robots.txt ./static/robots.txt
 docusaurus build
 cp ./env/staging.CNAME ./build/CNAME
 #cp ./env/staging.robots.txt ./build/robots.txt
