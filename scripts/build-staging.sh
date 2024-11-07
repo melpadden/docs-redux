778fe8aaeddf4628af4175fe421e4f789d76ef14
Mel Padden <melpadden@gmail.com>
2024-11-07 21:44:32 +0100

    Get algolia vars from root

--- scripts/build-staging.sh
+++ scripts/build-staging.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yarn clear
 cp ./env/staging.env ./.env
-cat ./env/algolia.env >> ./.env
+cat ./algolia.env >> ./.env
 #cp ./env/staging.CNAME ./CNAME
 cp ./env/staging.robots.txt ./static/robots.txt
 docusaurus build
