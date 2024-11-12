371f15dc46f3d6c987707e791199749ec50e3ae9
Mel Padden <melpadden@gmail.com>
2024-11-12 15:05:30 +0100

    Update index.md

--- scripts/build-dev.sh
+++ scripts/build-dev.sh
@@ -3,5 +3,5 @@ yarn clear
 cp ./env/development.env ./.env
 cat ./algolia.env >> ./.env
 cp ./env/development.robots.txt ./static/robots.txt
-docusaurus build
-cp ./env/development.CNAME ./build/CNAME
+# docusaurus build
+# cp ./env/development.CNAME ./build/CNAME
